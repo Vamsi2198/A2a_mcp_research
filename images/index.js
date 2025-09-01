2b76e91d61bd86cb71912b77df7ebebc9faf2ae5
Vamsi2198 <100421189+Vamsi2198@users.noreply.github.com>
2025-09-01 22:43:50 +0530

    Add files via upload

---
+++ images/index.js
@@ -0,0 +1,13 @@
+// Agent images array for Node.js compatibility
+const agentImages = [
+  './AgentHoodDP1.svg',
+  './AgentHoodDP2.svg',
+  './AgentHoodDP3.svg',
+  './AgentHoodDP4.svg',
+  './AgentHoodDP5.svg',
+  './AgentHoodDP6.svg',
+  './AgentHoodDP7.svg',
+  './AgentHoodDP8.svg',
+];
+
+module.exports = { agentImages }; 
---
+++ images/index.tsx
@@ -0,0 +1,23 @@
+
+// Import agent images
+import AgentHoodDP1 from './AgentHoodDP1.svg';
+import AgentHoodDP2 from './AgentHoodDP2.svg';
+import AgentHoodDP3 from './AgentHoodDP3.svg';
+import AgentHoodDP4 from './AgentHoodDP4.svg';
+import AgentHoodDP5 from './AgentHoodDP5.svg';
+import AgentHoodDP6 from './AgentHoodDP6.svg';
+import AgentHoodDP7 from './AgentHoodDP7.svg';
+import AgentHoodDP8 from './AgentHoodDP8.svg';
+
+const agentImages = [
+  AgentHoodDP1,
+  AgentHoodDP2,
+  AgentHoodDP3,
+  AgentHoodDP4,
+  AgentHoodDP5,
+  AgentHoodDP6,
+  AgentHoodDP7,
+  AgentHoodDP8,
+];
+
+export default agentImages;
